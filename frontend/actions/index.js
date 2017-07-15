f0cbbbdb0cc5195313529a79eb1f9fead6520727
fadececi <lunazhou96@gmail.com>
2017-07-14 17:35:40 -0700

    luna

--- frontend/actions/index.js
+++ frontend/actions/index.js
@@ -4,25 +4,25 @@ import * as types from './types';
 // time is an array of time objects that contains the key dayIndex with value (0 - 4 represent M - F)
 // and the key timeIndex with value (0-25 represent [8:00-8:30 AM] - [8:30 - 9:00 PM])
 
-const addClass = (classObj, time) => ({
+const addClass = (classObj) => ({
     type: types.ADD_CLASS,
     classObj,
     time
 });
 
-const deleteClass = (classObj, time) => ({
+const deleteClass = (classObj) => ({
     type: types.DELETE_CLASS,
     classObj,
     time
 });
 
-const highlightClass = (classObj, time) => ({
+const highlightClass = (classObj) => ({
     type: types.HIGHLIGHT_CLASS,
     classObj,
     time
 });
 
-const dehighlightClass = (classObj, time) => ({
+const dehighlightClass = (classObj) => ({
     type: types.DEHIGHLIGHT_CLASS,
     classObj,
     time
