bc9e9216a0752189d799892ba38189ef7e271fe3
git status <xiaoxie@wharton.upenn.edu>
2017-07-14 23:50:49 -0700

    timmy

--- frontend/containers/CalendarContainer.js
+++ frontend/containers/CalendarContainer.js
@@ -5,18 +5,19 @@ import * as actions from '../actions/index';
 
 const CalendarContainer = ({ calendarSchedule, addClass, deleteClass, highlightClass, dehighlightClass }) => {
     console.log(calendarSchedule);
-    let initialHour = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM','9PM'];
+    const initialHour = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM'];
     return (
         <div>
           <div className="calendarContainer">
             <div className="calendarColumn showHour">
-              <div>{initialHour.map((hour)=> (<div className='theHour'>{hour}</div>))}</div>
+              <div>{initialHour.map((hour)=> (<div className="theHour">{hour}</div>))}</div>
             </div>
             { calendarSchedule.map((daySchedule) => (
                     <div className="calendarColumn">{daySchedule.day}
                     {daySchedule.times.map((time) =>
-                     (time.chosen? <div className='time timeChosen'>{time.time}</div>:
-                       <div className='time timeNonChosen'>{time.time}</div>)
+                     (time.chosen ?
+                         <div className="time timeChosen">{time.time}</div> :
+                         <div className="time timeNonChosen">{time.time}</div>)
                     )}</div>
                 )
               )
